0f85a9ef185f55f9fd64958cc469877a606aa760
Arian Jahiri <ajahiri.AIT@gmail.com>
2021-08-15 15:06:58 +1000

    Backup of bad way to calculate global position for track components

--- Assets/Scripts/TrackSpawnerController.cs
+++ Assets/Scripts/TrackSpawnerController.cs
@@ -29,15 +29,6 @@ using UnityEngine;
 
 public class TrackSpawnerController : MonoBehaviour
 {
-    // Track Piece Prefabs, linked from Unity --> Old
-    // Using Resources.Load() instead so prefabs are known (required change as these prefabs will be referenced in the TrackPieceDeltas struct
-    // Doing that is not possible with a serliazed field because the prefab is not loaded until serialized causing (
-    //[SerializeReference] private Transform straightFlatLongPrefab;
-    //[SerializeReference] private Transform cornerFlatPrefab;
-
-    // Origin game object where spawning starts
-    private Transform spawnerTransformOrigin;
-
     // Delta Definitions, these don't change and are dependent on the 3D model of the piece
     private struct TrackPieceDeltas
     {
@@ -76,10 +67,6 @@ public class TrackSpawnerController : MonoBehaviour
         }
     }
 
-    // Track Delta definitions, xyz deltas need to be measured for each piece
-    //private TrackPieceDeltas straightFlatLongDeltas = ;
-    //private TrackPieceDeltas cornerFlatDeltas = ;
-
     // TrackPiece struct for use in list to track piece positions
     // Instantiations of track pieces need to have some of their properties accessible for spawning new pieces
     private struct TrackPiece
@@ -101,6 +88,13 @@ public class TrackSpawnerController : MonoBehaviour
         }
     }
 
+    /* 
+    * List for track pieces, might be better to have some fixed size data type and 
+    * only remember track pieces that are currently rendered in the view.
+    * Will utilise a stack for now.
+    */
+    private Stack<TrackPiece> trackPieceMemory = new Stack<TrackPiece>();
+
     /// <summary>
     /// Builds an array of all possible track deltas and loads the respective track piece prefab.
     /// </summary>
@@ -109,6 +103,7 @@ public class TrackSpawnerController : MonoBehaviour
     /// </returns>
     TrackPieceDeltas[] BuildDeltaDefinitions() {
         // Build the delta definitions within an array using Resourced.Load to load the prefab transforms
+        // Track Delta definitions, xyz deltas need to be measured for each piece
         TrackPieceDeltas[] trackDeltaDefs =  
         {
             new TrackPieceDeltas(-6.4f, -0.2f, -4f, -6.4f, -0.2f, 4f, 0, "none", 0, "none", "straight", Resources.Load<Transform>("TrackPieces/Straight_Flat_Long")),
@@ -120,40 +115,124 @@ public class TrackSpawnerController : MonoBehaviour
     void Start()
     {
         var deltaDefinitions = BuildDeltaDefinitions();
-        /* 
-         * List for track pieces, might be better to have some fixed size data type and 
-         * only remember track pieces that are currently rendered in the view.
-         */
-        var trackPieceMemory = new Stack<TrackPiece>();
 
-        spawnerTransformOrigin = GetComponent<Transform>();
+        // Origin game object where spawning starts
+        var spawnerTransformOrigin = GetComponent<Transform>();
 
-        // Instantiate the initial starting track piece where others will connect to
+        // Instantiate the initial starting track piece where subsequent pieces will connect to
         Instantiate(deltaDefinitions[0].prefab, new Vector3(spawnerTransformOrigin.position.x, spawnerTransformOrigin.position.y, spawnerTransformOrigin.position.z), Quaternion.identity);
-        var firstPiece = new TrackPiece(spawnerTransformOrigin.position.x, spawnerTransformOrigin.position.y, spawnerTransformOrigin.position.z, 0, Quaternion.identity);
-        trackPieceMemory.Push(firstPiece);
+        var firstPiece1 = new TrackPiece(spawnerTransformOrigin.position.x, spawnerTransformOrigin.position.y, spawnerTransformOrigin.position.z, 0, Quaternion.identity);
+        trackPieceMemory.Push(firstPiece1);
 
-        for (int i = 0; i < 1; i++)
+        int[] test = { 1, 0, 0, 1 , 0, 0, 1, 0, 0, 1 };
+
+        for (int i = 0; i < 6; i++)
         {
             var finalPiece = trackPieceMemory.Peek(); // Get last track piece
 
-            //int rand = Random.Range(0, 1);
-            int rand = 1;
+            //int rand = Random.Range(0, 2);
+            //int rand = 1;
+            int rand = test[i];
             var targetTrackDelta = deltaDefinitions[rand];
 
+            // Last piece orientation augmentation
+            // Determine the additional rotation to be added to new piece as a result of a turning piece
             // Adjust new piece positions based on last pos and end deltas
             // ADD END delta of final piece type to final piece pos and ADD START delta of new piece type aswell to get the correct origin coords for new piece
-            float newXPos = finalPiece.xPos + deltaDefinitions[finalPiece.deltasIndex].xDeltaEnd - targetTrackDelta.xDeltaStart;
-            float newYPos = finalPiece.yPos + deltaDefinitions[finalPiece.deltasIndex].yDeltaEnd - targetTrackDelta.yDeltaStart;
-            float newZPos = finalPiece.zPos + deltaDefinitions[finalPiece.deltasIndex].zDeltaEnd - targetTrackDelta.zDeltaStart;
+            float newXPos = finalPiece.xPos;
+            float newYPos = finalPiece.yPos;
+            float newZPos = finalPiece.zPos;
+            
+            Quaternion newPieceRotation = finalPiece.orientation;
+            var orientationDirection = deltaDefinitions[finalPiece.deltasIndex].orientationDirection;
+            switch (orientationDirection)
+            {
+                case "left":
+                    newPieceRotation *= Quaternion.Euler(0, -90, 0);
+                    break;
+                case "right":
+                    newPieceRotation *= Quaternion.Euler(0, 90, 0);
+                    break;
+                case "straight":
+                    break;
+                default:
+                    break;
+            }
 
-            TrackPiece newPiece = new TrackPiece(newXPos, newYPos, newZPos, rand, Quaternion.identity);
+            Instantiate(Resources.Load("Cube"), new Vector3(
+                        newXPos,
+            newYPos,
+            newZPos
+                    ), Quaternion.identity);
+
+            Debug.Log(newPieceRotation.eulerAngles.y);
+            if (newPieceRotation.eulerAngles.y == 270)
+            {
+                if (orientationDirection == "left")
+                {
+                    newXPos += deltaDefinitions[finalPiece.deltasIndex].xDeltaEnd + targetTrackDelta.zDeltaStart;
+                    newYPos += deltaDefinitions[finalPiece.deltasIndex].yDeltaEnd - targetTrackDelta.yDeltaStart;
+                    newZPos += deltaDefinitions[finalPiece.deltasIndex].zDeltaEnd - targetTrackDelta.xDeltaStart;
+                } else
+                {
+                    newXPos += -deltaDefinitions[finalPiece.deltasIndex].zDeltaEnd + targetTrackDelta.zDeltaStart;
+                    newYPos += deltaDefinitions[finalPiece.deltasIndex].yDeltaEnd - targetTrackDelta.yDeltaStart;
+                    newZPos += deltaDefinitions[finalPiece.deltasIndex].xDeltaEnd - targetTrackDelta.xDeltaStart;
+                }
+                
+            } 
+            else if(newPieceRotation.eulerAngles.y == 180)
+            {
+                if (orientationDirection == "left")
+                {
+                    newXPos += deltaDefinitions[finalPiece.deltasIndex].xDeltaEnd - targetTrackDelta.zDeltaStart;
+                    newYPos += deltaDefinitions[finalPiece.deltasIndex].yDeltaEnd - targetTrackDelta.yDeltaStart;
+                    newZPos += -deltaDefinitions[finalPiece.deltasIndex].zDeltaEnd + targetTrackDelta.xDeltaStart;
+                } else
+                {
+                    Instantiate(Resources.Load("Cube 1"), new Vector3(
+                        newXPos - deltaDefinitions[finalPiece.deltasIndex].xDeltaEnd - targetTrackDelta.xDeltaStart,
+                        newYPos + deltaDefinitions[finalPiece.deltasIndex].yDeltaEnd,
+                        newZPos + deltaDefinitions[finalPiece.deltasIndex].zDeltaEnd - targetTrackDelta.zDeltaStart
+                    ), Quaternion.identity);
+
+
+                    newXPos += -deltaDefinitions[finalPiece.deltasIndex].zDeltaEnd + targetTrackDelta.xDeltaStart;
+                    newYPos += deltaDefinitions[finalPiece.deltasIndex].yDeltaEnd - targetTrackDelta.yDeltaStart;
+                    newZPos += deltaDefinitions[finalPiece.deltasIndex].xDeltaEnd - targetTrackDelta.zDeltaStart;
+                    
+                }
+                
+            } 
+            else if (newPieceRotation.eulerAngles.y == 90)
+            {
+                if (orientationDirection == "left")
+                {
+                    newXPos += -deltaDefinitions[finalPiece.deltasIndex].xDeltaEnd + targetTrackDelta.zDeltaStart;
+                    newYPos += deltaDefinitions[finalPiece.deltasIndex].yDeltaEnd - targetTrackDelta.yDeltaStart;
+                    newZPos += -deltaDefinitions[finalPiece.deltasIndex].zDeltaEnd + targetTrackDelta.xDeltaStart;
+                }
+                else
+                {
+                    newXPos += -deltaDefinitions[finalPiece.deltasIndex].xDeltaEnd + targetTrackDelta.zDeltaStart;
+                    newYPos += deltaDefinitions[finalPiece.deltasIndex].yDeltaEnd - targetTrackDelta.yDeltaStart;
+                    newZPos += -deltaDefinitions[finalPiece.deltasIndex].zDeltaEnd + targetTrackDelta.xDeltaStart;
+                }
+            } 
+            else if (newPieceRotation.eulerAngles.y == 0)
+            {
+                newXPos += deltaDefinitions[finalPiece.deltasIndex].xDeltaEnd - targetTrackDelta.xDeltaStart;
+                newYPos += deltaDefinitions[finalPiece.deltasIndex].yDeltaEnd - targetTrackDelta.yDeltaStart;
+                newZPos += deltaDefinitions[finalPiece.deltasIndex].zDeltaEnd - targetTrackDelta.zDeltaStart;
+            }
+
+            TrackPiece newPiece = new TrackPiece(newXPos, newYPos, newZPos, rand, newPieceRotation);
 
             // Add new piece to the list
             trackPieceMemory.Push(newPiece);
 
             // Instantiate the prefab
-            Instantiate(targetTrackDelta.prefab, new Vector3(newXPos, newYPos, newZPos), Quaternion.identity);
+            Instantiate(targetTrackDelta.prefab, new Vector3(newXPos, newYPos, newZPos), newPieceRotation);
         }
     }
 
