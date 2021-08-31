0b010abb1a5c33ab4594f3ff5cccf6c994a94edc
Nathan13768609 <nathan.ibarcena@gmail.com>
2021-08-31 22:13:45 +1000

    Add RayCasters and Heuristics, and fix rewarding bug

--- Assets/Scripts/MoveToBallAgent.cs
+++ Assets/Scripts/MoveToBallAgent.cs
@@ -9,33 +9,35 @@ public class MoveToBallAgent : Agent
 {
     [SerializeField] private Transform targetTransform;
 
-    private Vector3 StartPosition = new Vector3(10.43f, 0.71f, -12.05f);
-
+    private Vector3 START_POSITION = new Vector3(10.43f, 0.71f, -12.05f);
     public override void OnEpisodeBegin()
     {
-        transform.position = StartPosition;
+        transform.position = START_POSITION;
     }
-    // WHAT DOES THIS DO!!!!
-    // public override void CollectObservations(VectorSensor sensor)
-    // {
-    //     sensor.AddObservation(transform.position);
-    //     sensor.AddObservation(targetTransform.position);
-    // }
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        sensor.AddObservation(transform.position);
+        sensor.AddObservation(targetTransform.position);
+    }
     public override void OnActionReceived(ActionBuffers actions) {
         GetComponent<CarController>().SetInputs(actions.ContinuousActions[0], 0f, actions.DiscreteActions[0] != 0);
     }
 
     public override void Heuristic(in ActionBuffers actionsOut)
     {
-        ActionSegment<float> continuouseActions = actionsOut.ContinuousActions;
+        ActionSegment<float> continuousActions = actionsOut.ContinuousActions;
+        continuousActions[0] = Input.GetAxis("Horizontal");
+
+        ActionSegment<int> discreteActions = actionsOut.DiscreteActions;
+        discreteActions[0] = Input.GetKey(KeyCode.Space) ? 1 : 0;
     }
     private void OnTriggerEnter(Collider other) {
         if (other.TryGetComponent<Ball>(out Ball ball)) {
-            SetReward(+1f);
+            AddReward(+1f);
             EndEpisode();
         }
         if (other.TryGetComponent<Wall>(out Wall wall)) {
-            SetReward(-1f);
+            AddReward(-1f);
             EndEpisode();
         }
     }
