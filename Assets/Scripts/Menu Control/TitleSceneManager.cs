d7c915d0510071aecdad37e8a1df61316b298af1
sudoersfile <tahscenery@gmail.com>
2021-10-14 00:32:42 +1100

    * Add options menu into `TitleScene`'s canvas * Change slider's text when its value is changed

--- Assets/Scripts/Menu Control/TitleSceneManager.cs
+++ Assets/Scripts/Menu Control/TitleSceneManager.cs
@@ -1,21 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class TitleSceneManager : MonoBehaviour
 {
+    [SerializeField] private TextMeshProUGUI botsCountSliderText;
+    [SerializeField] private TextMeshProUGUI gameVolumeSliderText;
+    [SerializeField] private TextMeshProUGUI difficultySliderText;
+
     public void StartGame()
     {
+        Vibrator.Vibrate(Vibration.SHORT);
         Debug.Log("Starting game...");
         SceneManager.LoadScene("RaceArea01");
-        Vibrator.Vibrate(Vibration.SHORT);  // 100 ms
     }
 
-    public void ViewOptions()
+    public void ChangeNumberOfBots(float newCountFloat)
     {
-        Debug.Log("Viewing options...");
-        SceneManager.LoadScene("OptionsScene");
-        Vibrator.Vibrate(Vibration.SHORT);  // 100 ms
+        Vibrator.Vibrate(Vibration.SHORT);
+        var newCount = (int)newCountFloat;
+        Debug.Log($"Setting bot count: {newCount}");
+        botsCountSliderText.text = newCount == 0 ? "None" : newCount.ToString();
+        PlayerPrefs.SetFloat("BotCount", newCount);
+    }
+
+    public void ChangeAudioVolume(float newVolume)
+    {
+        Vibrator.Vibrate(Vibration.SHORT);
+        Debug.Log($"Setting volume: {newVolume}");
+        AudioListener.volume = newVolume;
+        gameVolumeSliderText.text = $"{((int)newVolume)}%";
+        PlayerPrefs.SetFloat("GameVolume", newVolume);
     }
 }
