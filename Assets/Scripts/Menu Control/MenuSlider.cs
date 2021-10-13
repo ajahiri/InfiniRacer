eced986e9316c71e85fd421bc044dde50bff0c1e
sudoersfile <tahscenery@gmail.com>
2021-10-14 02:16:31 +1100

    * Implement Author screen * WIP Options menu fixes

---
+++ Assets/Scripts/Menu Control/MenuSlider.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class MenuSlider : MonoBehaviour
+{
+    [SerializeField] private Slider slider;
+    [SerializeField] private TextMeshProUGUI valueTextMesh;
+}
--- Assets/Scripts/Menu Control/TitleSceneManager.cs
+++ Assets/Scripts/Menu Control/TitleSceneManager.cs
@@ -1,15 +1,26 @@
-using System.Collections;
-using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using TMPro;
 
 public class TitleSceneManager : MonoBehaviour
 {
-    [SerializeField] private TextMeshProUGUI botsCountSliderText;
-    [SerializeField] private TextMeshProUGUI gameVolumeSliderText;
-    [SerializeField] private TextMeshProUGUI difficultySliderText;
+    [SerializeField] private TextMeshProUGUI botsCountTextMesh;
+    [SerializeField] private TextMeshProUGUI gameVolumeTextMesh;
+    [SerializeField] private TextMeshProUGUI difficultyTextMesh;
+
+    private int botsCount;
+    private float gameVolume;
+    private float difficulty;
+
+    public void Start()
+    {
+        botsCount = PlayerPrefs.GetInt("BotsCount", 1);
+        gameVolume = PlayerPrefs.GetFloat("GameVolume", 0.5f);
+        difficulty = PlayerPrefs.GetFloat("GameDifficulty", 0.5f);
+
+        UpdateBotCountSliderText();
+        UpdateGameVolumeSliderText();
+    }
 
     public void StartGame()
     {
@@ -21,18 +32,31 @@ public class TitleSceneManager : MonoBehaviour
     public void ChangeNumberOfBots(float newCountFloat)
     {
         Vibrator.Vibrate(Vibration.SHORT);
-        var newCount = (int)newCountFloat;
-        Debug.Log($"Setting bot count: {newCount}");
-        botsCountSliderText.text = newCount == 0 ? "None" : newCount.ToString();
-        PlayerPrefs.SetFloat("BotCount", newCount);
+        botsCount = (int)newCountFloat;
+
+        Debug.Log($"Setting bot count: {botsCount}");
+        UpdateBotCountSliderText();
+        PlayerPrefs.SetInt("BotCount", botsCount);
     }
 
     public void ChangeAudioVolume(float newVolume)
     {
         Vibrator.Vibrate(Vibration.SHORT);
-        Debug.Log($"Setting volume: {newVolume}");
-        AudioListener.volume = newVolume;
-        gameVolumeSliderText.text = $"{((int)newVolume)}%";
-        PlayerPrefs.SetFloat("GameVolume", newVolume);
+        gameVolume = newVolume / 100;
+
+        Debug.Log($"Setting volume: {(int)newVolume}%");
+        UpdateGameVolumeSliderText();
+        AudioListener.volume = gameVolume;
+        PlayerPrefs.SetFloat("GameVolume", gameVolume);
+    }
+
+    private void UpdateBotCountSliderText()
+    {
+        botsCountTextMesh.text = botsCount == 0 ? "None" : botsCount.ToString();
+    }
+
+    private void UpdateGameVolumeSliderText()
+    {
+        gameVolumeTextMesh.text = $"{gameVolume * 100}%";
     }
 }
