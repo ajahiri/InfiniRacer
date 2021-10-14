ebceacd973f1b6e3678c8d5c4eee0c62abc8dc7a
Arian Jahiri <arian.jahiri@student.uts.edu.au>
2021-10-14 19:08:00 +1100

    Added pregame screen
    - Added screen UI and scene management for pre game attention training feedback

--- Assets/Scripts/Menu Control/TitleSceneManager.cs
+++ Assets/Scripts/Menu Control/TitleSceneManager.cs
@@ -29,4 +29,18 @@ public class TitleSceneManager : MonoBehaviour
         SceneManager.LoadScene("OptionsScene");
         Vibrator.Vibrate(Vibration.SHORT);  // 100 ms
     }
+
+    public void PregameSubmission()
+    {
+        FindObjectOfType<AudioManager>().Play("button press");
+        SceneManager.LoadScene("AttentionScreen");
+        Vibrator.Vibrate(Vibration.SHORT);  // 100 ms
+    }
+
+    public void PostgameSubmission()
+    {
+        FindObjectOfType<AudioManager>().Play("button press");
+        SceneManager.LoadScene("PostAttentionScreen");
+        Vibrator.Vibrate(Vibration.SHORT);  // 100 ms
+    }
 }
---
+++ Assets/Scripts/PreGameForm.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PreGameForm : MonoBehaviour
+{
+    public string submissionName = "Anonymous";
+    public float attentionRating = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (attentionRating > 0)
+        {
+            GameObject.Find("SubmitPregameForm").GetComponent<Button>().interactable = true;
+        }
+    }
+
+    public void SetSubmissionName(string newSubmissionName)
+    {
+        submissionName = newSubmissionName;
+        Debug.Log(submissionName);
+    }
+    public void SetAttentionRating(float newAttentionRating)
+    {
+        attentionRating = newAttentionRating;
+        Debug.Log(attentionRating);
+    }
+
+    // Will save the submission details no matter what, when actual submission occurs
+    // in post game screen it will check if 0 rating or not
+    public void Proceed()
+    {
+        PlayerPrefs.SetFloat("attentionRatingPregame", attentionRating);
+        PlayerPrefs.SetString("submissionName", submissionName);
+        Debug.Log("Submitting pregame form...Saving data in prefs");
+        GameObject.Find("SceneManager").GetComponent<TitleSceneManager>().SelectCar();
+    }
+}
