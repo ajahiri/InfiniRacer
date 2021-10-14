72c2e5e21fd4331eaca2dc5a360c4255fc6a9069
Arian Jahiri <arian.jahiri@student.uts.edu.au>
2021-10-14 12:24:56 +1100

    Initial cloud functions for creating/reading sessions
    - Initial cloud functions for creating training sessions and reading single, reading all
    - Implementation is rough without authentication, this is by design and is meant to allow any user/device submit attention sessions
    - Implements create and read (one/all) attention sessions for use to monitor effectiveness of attention training of the application

--- GoogleCloud_Functions/functions/index.js
+++ GoogleCloud_Functions/functions/index.js
@@ -1,9 +1,64 @@
+const MongoClient = require('mongodb').MongoClient;
+const ObjectID = require('mongodb').ObjectId;
+const dbClient = new MongoClient('mongodb+srv://infiniracer:27x3VP7W90MGqYrf@infiniracer.ifeau.mongodb.net/InfiniRacer?retryWrites=true&w=majority', {useNewURLParser: true, useUnifiedTopology: true});
+
 const functions = require("firebase-functions");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// Create attention session
+exports.CreateAttentionSession = functions.region('australia-southeast1').https.onRequest(async (req,res) => {
+    const name = req.body.name || 'Anonymous';
+    const attentionBefore = req.body.attentionBefore;
+    const attentionAfter = req.body.attentionAfter;
+    const playTime = req.body.playTime;
+    try {
+        await dbClient.connect();
+        const db = dbClient.db('InfiniRacer');
+        const attentionSessions = db.collection("AttentionSessions");
+
+        const recordData = {
+            name,
+            attentionBefore,
+            attentionAfter,
+            playTime,
+        }
+
+        await attentionSessions.insertOne(recordData);
+
+        return res.send('Created attention session successfully!')
+    } catch (err) {
+        return res.send(err);
+    }
+});
+
+// Read attention session by id
+exports.ReadAttentionSession = functions.region('australia-southeast1').https.onRequest(async (req,res) => {
+    const sessionID = req.body.sessionID;
+    try {
+        await dbClient.connect();
+        const db = dbClient.db('InfiniRacer');
+        const attentionSessions = db.collection("AttentionSessions");
+
+        const allSessions = await attentionSessions.find().toArray();
+
+        const targetSession = await allSessions.findOne({_id: new ObjectID(sessionID)});
+
+        return res.status(200).send(targetSession);
+    } catch (err) {
+        return res.send(err);
+    }
+});
+
+// Get all attention sessions
+exports.ReadAllAttentionSession = functions.region('australia-southeast1').https.onRequest(async (req,res) => {
+    try {
+        await dbClient.connect();
+        const db = dbClient.db('InfiniRacer');
+        const attentionSessions = db.collection("AttentionSessions");
+
+        const allSessions = await attentionSessions.find().toArray();
+
+        return res.status(200).send({message: 'Found all sessions successfully!', sessions: allSessions});
+    } catch (err) {
+        return res.send(err);
+    }
+});
