9bfc9a27fd4721e907d91c15ff04a7eee1ab6bbb
Arian Jahiri <arian.jahiri@student.uts.edu.au>
2021-10-14 12:58:27 +1100

    Added serverless functions for adding user score and getting top scores
    - Added "Add score" function to add a new score to the DB
    - Added function to get top 5 scores from the DB
    - Added score to attention session doc

--- GoogleCloud_Functions/functions/index.js
+++ GoogleCloud_Functions/functions/index.js
@@ -9,6 +9,7 @@ exports.CreateAttentionSession = functions.region('australia-southeast1').https.
     const attentionBefore = req.body.attentionBefore;
     const attentionAfter = req.body.attentionAfter;
     const playTime = req.body.playTime;
+    const score = req.body.score || 0;
     try {
         await dbClient.connect();
         const db = dbClient.db('InfiniRacer');
@@ -19,6 +20,7 @@ exports.CreateAttentionSession = functions.region('australia-southeast1').https.
             attentionBefore,
             attentionAfter,
             playTime,
+            score
         }
 
         await attentionSessions.insertOne(recordData);
@@ -55,6 +57,42 @@ exports.ReadAllAttentionSession = functions.region('australia-southeast1').https
         const allSessions = await attentionSessions.find().toArray();
 
         return res.status(200).send({message: 'Found all sessions successfully!', sessions: allSessions});
+    } catch (err) {
+        return res.send(err);
+    }
+});
+
+// Add new user score (usually triggered at the end of a playthrough session)
+exports.AddNewScore = functions.region('australia-southeast1').https.onRequest(async (req,res) => {
+    const name = req.body.name || 'Anonymous';
+    const score = req.body.score || 0;
+    try {
+        await dbClient.connect();
+        const db = dbClient.db('InfiniRacer');
+        const highScores = db.collection("HighScores");
+
+        await highScores.insertOne({
+            name,
+            score
+        });
+
+        return res.status(200).send("Added new high score!");
+    } catch (err) {
+        return res.send(err);
+    }
+});
+
+// Get top 5 scores from the DB to show as the public high score table in game
+exports.GetTopScores = functions.region('australia-southeast1').https.onRequest(async (req,res) => {
+    try {
+        await dbClient.connect();
+        const db = dbClient.db('InfiniRacer');
+        const highScores = db.collection("HighScores");
+
+        // Get top 5 highest scores
+        const topFiveScores = await highScores.find().sort({score: -1}).limit(5).toArray();
+
+        return res.status(200).send({message: "Successfully found top 5 recorded high scores", topFiveScores});
     } catch (err) {
         return res.send(err);
     }
